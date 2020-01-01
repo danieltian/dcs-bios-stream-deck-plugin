51edb61db900056d82470c6a8ac9997ceb3fa6dc
Daniel Tian <dftian@gmail.com>
2019-12-31 21:54:28 -1000

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  env: { node: true },
+  extends: ['plugin:vue/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  }
+}
