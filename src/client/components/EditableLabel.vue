b2eae28fcb16fe14b52a1daffb038db736be7ba3
Daniel Tian <dftian@gmail.com>
2020-09-01 22:35:59 -1000

    Allow editable label to be keyboard driven

--- src/client/components/EditableLabel.vue
+++ src/client/components/EditableLabel.vue
@@ -1,6 +1,13 @@
 <template lang="pug">
   .editable-label.flex
-    input.input(type="text" ref="textbox" v-if="isEditing" :value="value" @input="emit" @blur="disableEditing")
+    input.input(
+      v-if="isEditing"
+      :value="value"
+      type="text"
+      ref="textbox"
+      @blur="save"
+      @keydown.enter="save"
+      @keydown.esc="cancel")
     label.label(v-else @dblclick="enableEditing") {{ value }}
 </template>
 
@@ -20,12 +27,17 @@
         this.$refs.textbox.select()
       },
 
-      disableEditing() {
+      cancel() {
         this.isEditing = false
       },
 
-      emit() {
-        this.$emit('input', this.value)
+      save(e) {
+        // If the user presses escape, cancel() will be called immediately followed by save(), because the input field
+        // has blurred. We can differentiate between an actual save and a cancel-then-save by checking isEditing.
+        if (this.isEditing === false) return
+
+        this.$emit('input', e.target.value)
+        this.isEditing = false
       },
     },
   }
